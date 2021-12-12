7704b52313095c65f327ba95af7aa8decd11e8b1
Lance Song <lance.song@redbubble.com>
2021-12-13 09:09:56 +1100

    day9

--- aoc2.cpp
+++ aoc2.cpp
@@ -9,59 +9,28 @@
 #include <queue>
 using namespace std;
 
-map<string, vector<string> > m;
+int m, n;
+
+int matrix[1001][1001];
+char line[1001];
 
 int res = 0;
 
-void dfs(string cur, vector<string> path)
+int tx[] = {0,0,1,-1};
+int ty[] = {1,-1,0,0};
+int flood(int i, int j)
 {
-    if(cur =="end")
+    if(matrix[i][j]==9)
+    return 0;
+    if(i<0||i>=m||j<0||j>=n)
+    return 0;
+    int ct = 1;
+    matrix[i][j] = 9;
+    for(int t=0; t<4; t++)
     {
-        res++;
-
-        // for(int i=0;i<path.size(); i++)
-        // {
-        //     cout<<path[i]<<" ";
-        // }
-        // cout<<endl;
-        return;
+        ct+= flood(i+tx[t], j+ty[t]);
     }
-    path.push_back(cur);
-
-    int flag = 0;
-    map<string, int> visit;
-    for(int i=0; i<path.size(); i++)
-    {
-        if(islower(path[i][0]))
-        {
-            if(visit[path[i]]==1)
-                flag = 1;
-        }
-        visit[path[i]]++;
-    }
-    for(int i=0; i <m[cur].size(); i++)
-    {
-        string t = m[cur][i];
-        if(t=="start")
-            continue;
-
-        if( islower(t[0]) )
-        {
-            if(flag ==1 && visit[t]<1)
-            {
-                dfs(t, path);
-            }
-            else if(flag ==0 && visit[t]<2)
-            {
-                dfs(t, path);
-            }
-        }
-        else
-        {
-            dfs(t, path);
-        }
-    }
-    return;
+    return ct;
 }
 int main()
 {
@@ -69,46 +38,34 @@ int main()
     freopen("input.txt", "r", stdin );
     freopen("output.txt", "w", stdout);
    
-   char str[11], st[101], ed[101];
-   while(scanf("%s",str)==1)
+    while(scanf("%s",line)==1)
+    {
+        n = strlen(line);
+        
+        for(int i=0;i<n;i++)
+        {
+            matrix[m][i]=line[i] - '0';
+        }
+        m++;
+    }
+   
+   vector<int> res;
+   for(int i=0;i<m;i++)
    {
-
-       int flag = 0;
-       string a = "", b= "";
-       for(int i =0; i< strlen(str); i++)
+       for(int j=0;j<n;j++)
        {
-            if(str[i]!='-'&&flag ==0)
-            {
-                a+= str[i];     
-              
-            } 
-            else if (str[i]=='-')
-            {
-                flag = 1;
-                continue;
-            }
-            else
-            {
-                b+=str[i];
-            }
-
+           if(matrix[i][j]!=9)
+            res.push_back(flood(i,j));
        }
-
-        m[a].push_back(b);
-        m[b].push_back(a);
-
-       
-       
    }
 
-        vector<string> em;
-        dfs("start", em);
-        
-        cout<<res<<endl;
-    
-    
-    
+   sort(res.begin(), res.end());
+//    for(int i=0;i<res.size();i++)
+//    cout<<res[i]<<endl;
+    int len = res.size();
+   cout<<res[len-1]*res[len-2]*res[len-3]<<endl;
 
+    
 
     return 0;
 }
