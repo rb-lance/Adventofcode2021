62ecdad329bc5f693d17ab837a316bdf06dcfc56
Lance Song <lance.song@redbubble.com>
2021-12-14 17:00:35 +1100

    day13

--- aoc2.cpp
+++ aoc2.cpp
@@ -9,62 +9,139 @@
 #include <queue>
 using namespace std;
 
-int m, n;
 
-int matrix[1001][1001];
-char line[1001];
 
-int res = 0;
+int mt[2001][2001];
+char str[1001];
+int ins=0;
+int m,n;
+int xy=0, line = 0;
 
-int tx[] = {0,0,1,-1};
-int ty[] = {1,-1,0,0};
-int flood(int i, int j)
+void convert(char ls[])
 {
-    if(matrix[i][j]==9)
-    return 0;
-    if(i<0||i>=m||j<0||j>=n)
-    return 0;
-    int ct = 1;
-    matrix[i][j] = 9;
-    for(int t=0; t<4; t++)
+    int len = strlen(ls);
+    if(ls[0]=='x')
+    xy = 0;
+    else xy = 1;
+    line = 0;
+    for(int i=2;i<len;i++)
     {
-        ct+= flood(i+tx[t], j+ty[t]);
+        line=line*10 + ls[i]-'0';
     }
-    return ct;
+}
+
+void fold()
+{
+    if(xy==1)
+    {
+        int a, b;
+        for(int i=0;i<=m;i++)
+        {
+            a= i;
+            for(int j=line+1;j<=n;j++)
+            {
+               
+                b = line - (j - line); 
+                mt[a][b]= max(mt[a][b], mt[i][j]);
+                mt[i][j] = 0;
+            }
+        }
+       n = line - 1;
+    }
+    else
+    {
+        int a, b;
+        for(int i=line+1;i<=m;i++)
+        {
+            a= line*2 - i;
+            for(int j=0;j<=n;j++)
+            {
+               
+                b = j; 
+
+                // cout<<a<<"ab"<<b<<"|"<<i<<"ij"<<j<<";";
+                mt[a][b]= max(mt[a][b], mt[i][j]);
+                mt[i][j] = 0;
+            }
+        }
+        m = line - 1;
+       
+    }
+}
+
+int check()
+{
+    int ans = 0;
+    for(int i=0; i<=m;i++)
+    {
+        for(int j=0;j<=n;j++)
+        {
+            if(mt[i][j]==1)
+            {
+                ans++;
+                // cout<<'#';
+            }
+            else
+            {
+                // cout<<'.';
+            }
+        }
+        // cout<<endl;
+    }
+    return ans;
+}
+
+void print()
+{
+   
+    for(int j=0;j<=n;j++)
+    {
+        for(int i=0; i<=m;i++)
+        {
+            if(mt[i][j]==1)
+            {
+              
+                cout<<'#';
+            }
+            else
+            {
+                cout<<'.';
+            }
+        }
+        cout<<endl;
+    }
 }
 int main()
 {
 
     freopen("input.txt", "r", stdin );
     freopen("output.txt", "w", stdout);
-   
-    while(scanf("%s",line)==1)
-    {
-        n = strlen(line);
-        
-        for(int i=0;i<n;i++)
-        {
-            matrix[m][i]=line[i] - '0';
-        }
-        m++;
-    }
-   
-   vector<int> res;
-   for(int i=0;i<m;i++)
+   memset(mt, 0, sizeof(mt));
+   int a,b;
+   n = 0, m = 0;
+   while(scanf("%d,%d", &a,&b)==2)
    {
-       for(int j=0;j<n;j++)
-       {
-           if(matrix[i][j]!=9)
-            res.push_back(flood(i,j));
-       }
+    //    cout<<a<<" "<<b<<endl;
+    m = max(a, m);
+    n = max(b, n);
+       mt[a][b]=1;
    }
 
-   sort(res.begin(), res.end());
-//    for(int i=0;i<res.size();i++)
-//    cout<<res[i]<<endl;
-    int len = res.size();
-   cout<<res[len-1]*res[len-2]*res[len-3]<<endl;
 
+    cout<<m<<" "<<n<<endl;
+    
+   while(scanf("%*s %*s %s",str)==1)
+   {
+    convert(str);
+    cout<<xy<<" "<<line<<endl;
+    
+    fold();
+    print();
+    cout<<m<<"<mn>"<<n<<endl;
+    cout<<check()<<endl;;
+    
+   }
+  
     
 
     return 0;
